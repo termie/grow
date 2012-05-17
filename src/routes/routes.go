1d636d35e39855a9bc49f1bf0aa443c31d683650
termie <github@anarkystic.com>
2012-05-17 16:46:12 +0800

    added router

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -6,23 +6,55 @@ package routes
 import (
   "path"
   "regexp"
+  "net/http"
 )
 
 var dynamic_matcher = regexp.MustCompile(`\{([\w_]+)\}`)
 
 type Route struct {
+  Handler http.Handler
   Matcher *regexp.Regexp
   Names []string
 }
 
-func Compile (route string) (*Route) {
-  clean_route := path.Clean(route)
-  names := dynamic_matcher.FindAllStringSubmatch(clean_route, -1)
+
+func Compile (url_path string, handler http.Handler) (*Route) {
+  clean_path := path.Clean(url_path)
+
+  // Extract the names of the params
+  names := dynamic_matcher.FindAllStringSubmatch(clean_path, -1)
   size := len(names)
   names_out := make([]string, size)
   for i, v := range names {
     names_out[i] = v[1]
   }
-  repl := dynamic_matcher.ReplaceAllLiteralString(clean_route, `([\w_]+)`)
-  return &Route{regexp.MustCompile(repl), names_out}
+
+  // Generate a matcher regexp
+  repl := dynamic_matcher.ReplaceAllLiteralString(clean_path, `([\w_]+)`)
+  return &Route{handler, regexp.MustCompile(repl), names_out}
+}
+
+func (route *Route) Match (url_path string) (bool) {
+  return route.Matcher.MatchString(url_path)
+}
+
+
+type Router struct {
+  Routes []*Route
+}
+
+func (router *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+  var matched_route *Route
+
+  for _, matcher := range router.Routes {
+    if matcher.Match(req.URL.Path) {
+      matched_route = matcher
+      break
+    }
+  }
+  if matched_route == nil {
+    http.Error(w, "No route found for that path.", http.StatusNotFound)
+    return
+  }
+  matched_route.Handler.ServeHTTP(w, req)
 }
--- t.go
+++ t.go
@@ -3,11 +3,27 @@ package main
 import (
   "routes"
   "fmt"
+  "net/http"
+  "flag"
+  "log"
 )
 
+var addr = flag.String("addr", ":8080", "http service address")
+
 func main() {
-  a := routes.Compile("/foo/{bar}")
-  fmt.Println(a.Matcher)
-  fmt.Println(a.Names)
+  flag.Parse()
+  f := http.HandlerFunc(func (w http.ResponseWriter, req *http.Request) {
+    fmt.Fprintf(w, "hello")
+  })
+
+  a := routes.Compile("/foo/{bar}", f)
+  router := new(routes.Router)
+  router.Routes = []*routes.Route{a}
+
+  http.Handle("/", router)
+  err := http.ListenAndServe(*addr, nil)
+  if err != nil {
+      log.Fatal("ListenAndServe:", err)
+  }
 
 }
